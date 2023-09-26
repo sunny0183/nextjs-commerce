e8baa4ece6d13a098486d029f84315b246aa460a
gitName <a@b.c>
2023-09-26 12:21:46 +0100

    fix OG error

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import OpengraphImage from 'components/opengraph-image';
-import { EdgeOrNode } from 'lib/utils';
+//import { EdgeOrNode } from 'lib/utils';
 
-export const runtime = EdgeOrNode();
+//export const runtime = EdgeOrNode();
 
 export default async function Image() {
   return await OpengraphImage();
